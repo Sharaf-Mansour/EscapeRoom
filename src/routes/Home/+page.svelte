558ae6fe0c660b3dbb50afa661c1066b956f6682
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-10-27 17:05:48 +0300

    .

--- src/routes/Home/+page.svelte
+++ src/routes/Home/+page.svelte
@@ -3,95 +3,105 @@
 <script>
         import {goto} from "$app/navigation";
         const videoSrc = `${import.meta.env.BASE_URL}video.mp4`;
+        var loadingindicator = false ; 
+        import { ProgressRadial } from '@skeletonlabs/skeleton';
+        function loading() {
+          loadingindicator = true 
+        }
 </script>
 
 
-{#if !videoSrc}
+{#if !loadingindicator}
   
+
 <div class="h-screen home flex items-center justify-center bg-black ">
 	<ProgressRadial />
 </div>
   
-  
-  <style>
-   .body{
+<style>
+     .body{
 	 display:none;
    }
-  </style>
-
-{:else}
-<section  class="flex items-center justify-center mt-3 ">
-  <div class="">
-    <video loading="lazy" preload="auto"   src={videoSrc}  controls  class="w-full rounded-lg "></video>
-  </div>
-</section>
-
-
-<section class=" intro flex items-center justify-center h-80 mt-5">
-  <div class="text-center">
-   <h2 class="h3 p-5 xl:px-56">Welcome to Haunted, where reality merges with nightmares. Dive into a world of chilling mysteries and spine-tingling puzzles that test your courage and wit. Can you uncover the dark secrets before time runs out? Enter if you dare, but beware: not all who enter Haunted make it back!</h2>
-  </div>
-  </section>
-
-
-  <!-- how to play  -->
-  <section class="Howtoplay  px-5 xl:px-20 mb-28">
-      <h1 class="text-center h1 font-bold mt-24 mb-24">How To Play</h1>
-      <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3 xl:gap-2 md:gap-2">
-        
-        <div>
-          <div class="card mb-5 h-96">
-            <header>
-              <img src="how1.jpg" class="w-full h-48 object-cover bg-black/50 rounded-md" alt="Post" />
-            </header>
-            <div class="card-header text-center">
-              <h2 class="h2">Step 1</h2>
-            </div>
-            <div class="card-header">
-              <h2 class="h5">You and your team will choose one of our cool themes. You can choose to experience the game with horror or you can play the classic escape room experience with no horror.</h2>
-            </div>
-          </div>
-        </div>
-    
-        <div>
-          <div class="card mb-5 h-96">
-            <header>
-              <img src="how2.jpg" class="w-full h-48 object-cover bg-black/50 rounded-md" alt="Post" />
-            </header>
-            <div class="card-header text-center">
-              <h2 class="h2">Step 2</h2> <!-- Changed to Step 2 for clarity -->
-            </div>
-            <div class="card-header">
-              <h2 class="h5">You and your team will choose one of our cool themes. You can choose to experience the game with horror or you can play the classic escape room experience with no horror.</h2>
-            </div>
-          </div>
-        </div>
-    
-        <div>
-          <div class="card h-96">
-            <header>
-              <img src="how3.jpg" class="w-full h-48 object-cover bg-black/50 rounded-md" alt="Post" />
-            </header>
-            <div class="card-header text-center">
-              <h2 class="h2">Step 3</h2> <!-- Changed to Step 3 for clarity -->
-            </div>
-            <div class="card-header text-center">
-              <h2 class="h5">We guarantee a fun time, can you ESCAPE?</h2>
-              <br>
-              <button class="bg-primary-900 rounded-md px-5 py-2">
-                  <h2  onclick={() => goto("/Themes")} class="h5 cursor-pointer">See Themes => </h2>
-              </button>
-            </div>
-          </div>
-        </div>
-    
-      </div>
-    </section>
-    
+</style>
 
 
 {/if}
 
+<section class="body">
+
+
+
+  <section  class="flex items-center justify-center mt-3 ">
+    <div class="">
+      <video onloadeddata={loading} loading="lazy" preload="auto"   src={videoSrc}  controls  class="w-full rounded-lg "></video>
+    </div>
+  </section>
+  
+  
+<section class=" intro flex items-center justify-center h-80 mt-5">
+    <div class="text-center">
+     <h2 class="h3 p-5 xl:px-56">Welcome to Haunted, where reality merges with nightmares...</h2>
+    </div>
+    </section>
+
+
+    <!-- how to play  -->
+    <section class="Howtoplay mt-5 px-5 xl:px-20 mb-28">
+        <h1 class="text-center h1 font-bold mt-24 mb-24">How To Play</h1>
+        <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3 xl:gap-2 md:gap-2">
+          
+          <div>
+            <div class="card mb-5 h-96">
+              <header>
+                <img onloadeddata={loading} src="how1.jpg" class="w-full h-48 object-cover bg-black/50 rounded-md" alt="Post" />
+              </header>
+              <div class="card-header text-center">
+                <h2 class="h2">Step 1</h2>
+              </div>
+              <div class="card-header">
+                <h2 class="h5">You and your team will choose one of our cool themes. You can choose to experience the game with horror or you can play the classic escape room experience with no horror.</h2>
+              </div>
+            </div>
+          </div>
+      
+          <div>
+            <div class="card mb-5 h-96">
+              <header>
+                <img onloadeddata={loading} src="how2.jpg" class="w-full h-48 object-cover bg-black/50 rounded-md" alt="Post" />
+              </header>
+              <div class="card-header text-center">
+                <h2 class="h2">Step 2</h2> <!-- Changed to Step 2 for clarity -->
+              </div>
+              <div class="card-header">
+                <h2 class="h5">You and your team will choose one of our cool themes. You can choose to experience the game with horror or you can play the classic escape room experience with no horror.</h2>
+              </div>
+            </div>
+          </div>
+      
+          <div>
+            <div class="card h-96">
+              <header>
+                <img onloadeddata={loading} src="how3.jpg" class="w-full h-48 object-cover bg-black/50 rounded-md" alt="Post" />
+              </header>
+              <div class="card-header text-center">
+                <h2 class="h2">Step 3</h2> <!-- Changed to Step 3 for clarity -->
+              </div>
+              <div class="card-header text-center">
+                <h2 class="h5">We guarantee a fun time, can you ESCAPE?</h2>
+                <br>
+                <button class="bg-primary-900 rounded-md px-5 py-2">
+                    <h2  onclick={() => goto("/Themes")} class="h5 cursor-pointer">See Themes => </h2>
+                </button>
+              </div>
+            </div>
+          </div>
+      
+        </div>
+      </section>
+      
+ 
+
+</section>
 
       
 
--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -1,22 +1,42 @@
 <script>
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-
+	var loadingindicator = $state(false) ; 
+        import { ProgressRadial } from '@skeletonlabs/skeleton';
+        function loading() {
+          loadingindicator = true 
+        }
 	$effect(() => {
 		AOS.init({ duration: 1000 }); // Adjust duration as needed
 	});
 </script>
 
+{#if loadingindicator}
+  
+
+<div class="h-screen home flex items-center justify-center bg-black ">
+	<ProgressRadial />
+</div>
+  
+<style>
+     .home{
+	 display:none;
+   }
+</style>
+
+
+{/if}
 <section class="h-screen home flex items-center justify-center">
 	<div class="text-center">
 		<h2 data-aos="fade-right" class="h1 font-bold">Welcome to</h2>
 		<h1 data-aos="fade-right" class="h1 font-extrabold">Haunted Escape Room</h1>
 		<img
-		    preload="auto"
+		    
 			data-aos="fade-up"
 			src="/logo.png"
 			alt="Logo"
 			class="mx-auto w-60 mt-3 mb-3"
+			onloadeddata={loading}
 		/>
 		<button
 			data-aos="fade-down"
